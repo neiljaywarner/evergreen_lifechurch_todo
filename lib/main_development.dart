12385bb2c3c2fec0e934158594d39c79c4e34f0b
neil warner <njwandroid@gmail.com>
2024-10-06 12:10:22 -0500

    1 - example for firebase auth works enough to create an account with email/pass

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,56 @@
 import 'package:evergreen_lifechurch_todo/app/app.dart';
 import 'package:evergreen_lifechurch_todo/bootstrap.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  bootstrap(() => const App());
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+
+import 'firebase_options_dev.dart';
+
+Future<void> main() async {
+ // bootstrap(() => const App());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final providers = [EmailAuthProvider()];
+
+    return MaterialApp(
+      initialRoute: FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
+      routes: {
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: providers,
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/profile');
+              }),
+            ],
+          );
+        },
+        '/profile': (context) {
+          return ProfileScreen(
+            providers: providers,
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/sign-in');
+              }),
+            ],
+          );
+        },
+      },
+    );
+  }
 }
